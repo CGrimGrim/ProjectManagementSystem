124307ba164d13430433f96b226a020ba29a1c31
SWTT <cgrimward.dwp@gmail.com>
2015-05-19 13:22:16 +0100

    Changes

--- build/web/index.jsp
+++ build/web/index.jsp
@@ -1,3 +1,109 @@
+<<<<<<< HEAD
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Menu</title>
+<link rel="stylesheet" type = "text/css" href="projectManagement.css">
+<link rel="stylesheet" type="text/css" href="dropDown.css">
+</head>
+<body>
+<header>
+<h1>Project Management System</h1>
+</header>
+<div class="split"><br></div>
+<nav class="menu">
+<ul>
+
+<li><a href="#">Staff</a>
+
+	<ul>
+	
+	<li><a href="allStaff">View All Staff Members</a></li>
+	
+	<li><a href="">Update Staff Members Details</a>
+		<ul>
+		<li><form class="dropDown" action="updateStaff.jsp">
+		Please Enter The Staff ID:
+		<input type="text" name="staffID">
+		<input type="submit" value="Submit">
+		<br>
+		<br>
+		</form></li>
+		</ul></li>
+
+	<li><a href="assignStaff.jsp">Assign Employee To Project</a></li>
+	
+	<li><a href="unassignStaff.jsp">Un-assign Employee</a></li>
+	
+	<li><a href="newStaff.jsp">Add New Member Of Staff</a></li>
+	
+	<li><a href="">Remove Member Of Staff</a>
+		<ul>
+		<li><form class="dropDown" action="deleteEmployee" method="Get">
+                    Please Enter The Staff ID:
+                    <input type="text" name="staffID">
+                    <input type="submit" value="Submit">
+		<br>
+		<br>
+		</form></li>
+		</ul></li>
+	</ul>	
+</li>
+
+
+<li><a href="#">Projects</a>
+	<ul>
+	<li><a href="#">Update A Project</a>
+		<ul>
+		<li><form class="dropDown" action="updateProject.jsp">
+		Please Enter The Project ID:
+		<input type="text" name="projectID">
+		<input type="submit" value="Submit">
+		<br>
+		<br>
+		</form></li>
+		</ul></li>
+	<li><a href="newProject.jsp">Add New Project</a></li>
+	<li><a href="">Delete A Project</a>
+	<ul>
+		<li><form class="dropDown" action="deleteProject.jsp">
+		Please Enter The Project ID:
+		<input type="text" name="projectID">
+		<input type="submit" value="Submit">
+		<br>
+		<br>
+		</form></li>
+		</ul></li>
+	</ul>
+</li>
+
+
+<li><a href="#">Account</a>
+
+	<ul>
+	<li><a href="">My Account</a></li>
+	<li><a href="">Change Password</a></li>
+	<li><a href="logIn.jsp">Log Out</a></li> 
+	</ul>
+</li>
+
+<li><form action="#">
+	Search:&nbsp;<select><option value="staff">Staff</option>
+						 <option value="projects">Projects</option>
+				 </select>
+	By:&nbsp;<select><option value="staffID">Staff ID</option>
+					 <option value="projectID">Project ID</option>
+					 <option value="name">Name</option>
+					 <option value="location">Location</option>
+			 </select>
+	&nbsp;&nbsp;<input type="search" name="id">
+	<input type="submit" value="Submit">
+	</form></li>
+</ul>
+</nav>
+</body>
+=======
 <!DOCTYPE html>
 <html>
     <head>
@@ -101,4 +207,5 @@
 </nav>
 <img src="dwp-logo.jpg">
 </body>
+>>>>>>> 1b514216901df361493a367b00894599effe32ae
 </html>
--- src/java/dwp/resource/management/data/DataLink.java
+++ src/java/dwp/resource/management/data/DataLink.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
 
  * To change this license header, choose License Headers in Project Properties.
@@ -35,95 +34,6 @@ public class DataLink {
     	callstat.setString(2, p.getName());
     	callstat.setInt(3, p.getTotalHours());
     	callstat.setString(4, p.getLocationID());
-    	callstat.setString(5, p.getPhase());
-    	callstat.setDate(6, Date.valueOf(p.getStartDate()));
-    	callstat.setDate(7, Date.valueOf(p.getEndDate()));
-    }
-           
-    public void deleteEmployee(String id) throws SQLException{
-       callstat = connect.prepareCall("Call deleteEmployee(?)");        
-        callstat.setString(1, id );
-        callstat.execute(); 
-    }
-    
-    public void addEmployee(Employee e) throws SQLException{
-        callstat = connect.prepareCall("call addEmployee(?,?,?,?,?,?,?,?)");
-        callstat.setInt(1, e.getEmployeeID());
-        callstat.setString(2, e.getFirstName());
-        callstat.setString(3, e.getLastName());
-        callstat.setDate(4, Date.valueOf(e.getStartDate()));
-        callstat.setDate(5, Date.valueOf(e.getEndDate()));
-        callstat.setString(6, e.getContactNumber());
-        callstat.setString(7, e.getContractType());
-        callstat.setDouble(8, e.getContractedHours());
-        callstat.executeQuery();
-    }
-    
-    public void updateEmployee(Employee e) throws SQLException{
-        callstat = connect.prepareCall("call updateEmployee(?,?,?,?,?,?,?,?)");
-        callstat.setInt(1, e.getEmployeeID());
-        callstat.setString(2, e.getFirstName());
-        callstat.setString(3, e.getLastName());
-        callstat.setDate(4, Date.valueOf(e.getStartDate()));
-        callstat.setDate(5, Date.valueOf(e.getEndDate()));
-        callstat.setString(6, e.getContactNumber());
-        callstat.setString(7, e.getContractType());
-        callstat.setDouble(8, e.getContractedHours());
-        callstat.executeQuery();
-        
-    }
-    
-    public ResultSet getUserInformation(String username) throws SQLException{
-        callstat = connect.prepareCall("call retrieveUserInformation(?)");
-        callstat.setString(1, username);
-        return callstat.executeQuery();
-    }
-    
-    public ResultSet getEmployee(int employeeID) throws SQLException{
-        callstat = connect.prepareCall("call searchEmployeeByID(?)");
-        callstat.setInt(1, employeeID);
-        return callstat.executeQuery();
-    }
-    
-    public ResultSet getAllStaff() throws SQLException{
-    	ResultSet rs = statement.executeQuery("SELECT * from employee");
-    	return rs;
-    }
-    
-}
-=======
-/*
-
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package dwp.resource.management.data;
-
-/**
- *
- * @author 10071639
- */
-import dwp.resource.management.objects.*;
-import java.sql.*;
-
-public class DataLink {
-    
-    private Connection connect = null;
-    private CallableStatement callstat = null;
-    
-    public DataLink(String databaseLocation, String username, String password) throws ClassNotFoundException, SQLException{
-        Class.forName("com.mysql.jdbc.Driver");
-        connect = DriverManager.getConnection(databaseLocation, username, password);
-    }
-    
-      
-    public void addProject(Project p) throws SQLException {
-    	callstat = connect.prepareCall("call addProject(?,?,?,?,?,?,?)");
-    	callstat.setInt(1, p.getProjectID());
-    	callstat.setString(2, p.getName());
-    	callstat.setDouble(3, p.getTotalHours());
-    	callstat.setString(4, p.getLocationName());
     	callstat.setString(5, p.getPhase());
     	callstat.setDate(6, Date.valueOf(p.getStartDate()));
     	callstat.setDate(7, Date.valueOf(p.getEndDate()));
@@ -175,17 +85,14 @@ public class DataLink {
         return callstat.executeQuery();
     }
     
-    public ResultSet getProject(int projectID) throws SQLException{
-        callstat = connect.prepareCall("Call searchProjectByID(?)");
-        callstat.setInt(1, projectID);
-        return callstat.executeQuery();
-    }
-
-    public void deleteProject(int projectID) throws SQLException{
-        callstat = connect.prepareCall("Call deleteProject(?)");
-        callstat.setInt(1, projectID);
-        callstat.executeQuery();
+    public ResusltSet getAllStaff() throws SQLException{
+    	ResultSet rs = statement.executeQuery("SELECT * from employee");
+    	return rs;
     }
     
+    public void deleteProject(int projectID) throws SQLException{
+        callstat = connect.prepareCall("call deleteProject(?)");
+        callstat.setInt(1, projectID);
+        callstat.execute();
+    }
 }
->>>>>>> 1b514216901df361493a367b00894599effe32ae
